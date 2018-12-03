e615122fa0b75728445ca084bb80d0e39e0200ed
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-12-03 10:41:04 -0800

    goff lab loading code

--- geneselection/datasets/mouse_retina/load.py
+++ geneselection/datasets/mouse_retina/load.py
@@ -26,29 +26,41 @@ def download_file(url, loc="data_files", blocksize=1024):
     return local_filename
 
 
-def load(files={"10x_mouse_retina_development.mtx":"https://www.dropbox.com/s/6d76z4grcnaxgcg/10x_mouse_retina_development.mtx?dl=1",
-                "10x_mouse_retina_development_phenotype.csv":"https://www.dropbox.com/s/y5lho9ifzoktjcs/10x_mouse_retina_development_phenotype.csv?dl=1",
-                "10x_mouse_retina_development_feature.csv":"https://www.dropbox.com/s/1mc4geu3hixrxhj/10x_mouse_retina_development_feature.csv?dl=1"}
-         loc="data_files"):
+def load(loc="data_files", blocksize=1024, anndata_write=True, anndata_name="mouse_retina"):
     
-    # download files if they don't exist locally
-    if not os.path.exists(loc):
-        os.makedirs(loc)
-    for fname, url in files.items():
-        if not os.path.exists(os.path.join(loc,fname)):
-            _ = download_file(loc=loc)
+    adata_fpath = os.path.join(loc, anndata_name)
+    
+    # if we've already down;loaded and constructed the adata file, read it and use it
+    if os.path.exists(adata_fpath) and os.path.isfile(adata_fpath):
+        adata = sc.read_h5ad(adata_fpath)
 
-    # read in data
-    df_obs = pd.read_csv("data/10x_mouse_retina_development_phenotype.csv", index_col=0)[
-    ["barcode", "sample", "age", "CellType"]]
-    df_var = pd.read_csv("data/10x_mouse_retina_development_feature.csv", index_col=0)[
-    ["id", "gene_short_name"]]
-    count_mat = mmread("data/10x_mouse_retina_development.mtx")
+    # if anndata doesn't exit alread, download inputs and construct it
+    else:
+        # download files if they don't exist locally
+        if not os.path.exists(loc):
+            os.makedirs(loc)
+        files={"10x_mouse_retina_development.mtx":"https://www.dropbox.com/s/6d76z4grcnaxgcg/10x_mouse_retina_development.mtx?dl=1",
+               "10x_mouse_retina_development_phenotype.csv":"https://www.dropbox.com/s/y5lho9ifzoktjcs/10x_mouse_retina_development_phenotype.csv?dl=1",
+               "10x_mouse_retina_development_feature.csv":"https://www.dropbox.com/s/1mc4geu3hixrxhj/10x_mouse_retina_development_feature.csv?dl=1"}
+        for fname, url in files.items():
+            if not os.path.exists(os.path.join(loc,fname)):
+                _ = download_file(loc=loc, blocksize=blocksize)
 
-    # make anndata object
-    adata = sc.AnnData(X=count_mat.toarray().transpose(), obs=df_obs, var=df_var)
-    genes_to_keep = np.mean(adata.X != 0, axis=0) > 0
-    cells_to_keep = np.mean(adata.X != 0, axis=1) > 0
-    adata = adata[:, genes_to_keep][cells_to_keep, :].copy()
+        # read in data
+        df_obs = pd.read_csv("data/10x_mouse_retina_development_phenotype.csv", index_col=0)[
+        ["barcode", "sample", "age", "CellType"]]
+        df_var = pd.read_csv("data/10x_mouse_retina_development_feature.csv", index_col=0)[
+        ["id", "gene_short_name"]]
+        count_mat = mmread("data/10x_mouse_retina_development.mtx")
 
+        # make anndata object
+        adata = sc.AnnData(X=count_mat.toarray().transpose(), obs=df_obs, var=df_var)
+        genes_to_keep = np.mean(adata.X != 0, axis=0) > 0
+        cells_to_keep = np.mean(adata.X != 0, axis=1) > 0
+        adata = adata[:, genes_to_keep][cells_to_keep, :].copy()
+
+        # save a local copy
+        if write_anndata:
+            adata.write(adata_fpath)
+    
     return adata
