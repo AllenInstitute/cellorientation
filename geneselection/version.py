0b0a41f8d48bf283d292eb0bb6a1826c78459169
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-12-03 11:47:41 -0800

    fixed typo

--- geneselection/version.py
+++ geneselection/version.py
@@ -22,7 +22,7 @@ CLASSIFIERS = [
     "Intended Audience :: Science/Research",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
-    "Programming Language :: Python",ss
+    "Programming Language :: Python",
     "Topic :: Scientific/Engineering",
 ]
 
