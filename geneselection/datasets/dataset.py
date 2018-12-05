d5b4c53b3e5902820ff05fa19527bed6b9bd881e
Gregory Johnson <gregj@alleninstitute.org>
2018-12-04 16:57:40 -0800

    at least it works

--- geneselection/datasets/dataset.py
+++ geneselection/datasets/dataset.py
@@ -5,8 +5,11 @@ from torch import Tensor
 
 import scanpy.api as sc
 
+
 class GSDataset(Dataset):
-    def __init__(self, data: anndata.AnnData, as_tensor: bool=True, with_obj_label: bool=True):
+    def __init__(
+        self, data: anndata.AnnData, as_tensor: bool = True, with_obj_label: bool = True
+    ):
         """
         A data provider class for the larger project. The idea is to capture an AnnData and then
         serve up the rows as either pytorch Tensors, or numpy ndarrays based on the keyword argument
@@ -16,13 +19,15 @@ class GSDataset(Dataset):
         """
         super(GSDataset, self).__init__()
         self.data = data.copy()
-        self.return_tensor=as_tensor
-        self.with_obj_label=with_obj_label
+        self.return_tensor = as_tensor
+        self.with_obj_label = with_obj_label
 
     def __len__(self) -> int:
         return self.data.n_obs
 
-    def __getitem__(self, index: int) -> [ndarray, Tensor, (ndarray, ndarray), (ndarray, Tensor)]:
+    def __getitem__(
+        self, index: int
+    ) -> [ndarray, Tensor, (ndarray, ndarray), (ndarray, Tensor)]:
         row = None
         if not self.return_tensor:
             row = self.data.X[index]
@@ -30,13 +35,17 @@ class GSDataset(Dataset):
             row = Tensor(self.data.X[index])
         if not self.with_obj_label:
             return row
-        keys = self.data.obs[index]
+        keys = self.data.obsm[index]
         return (keys, row)
 
     def __add__(self, other):
         self.data.concatenate(other)
 
-def get_aics_rnaseq(path = '/allen/aics/gene-editing/RNA_seq/scRNAseq_SeeligCollaboration/data_for_modeling/scrnaseq_cardio_20181129.h5ad', **kwargs):
+
+def get_aics_rnaseq(
+    path="/allen/aics/gene-editing/RNA_seq/scRNAseq_SeeligCollaboration/data_for_modeling/scrnaseq_cardio_20181129.h5ad",
+    **kwargs
+):
     adata = sc.read(path)
-    
+
     return GSDataset(adata, **kwargs)
