b8c19b9b70bfbc82d74b655d0cf0ffb91f9c367f
Gregory Johnson <gregj@alleninstitute.org>
2018-12-04 16:57:19 -0800

    more simple simple logger

---
+++ geneselection/simplelogger.py
@@ -0,0 +1,25 @@
+class SimpleLogger:
+    def __init__(self, print_format):
+
+        self.print_format = print_format
+
+        self.log = dict()
+
+    def add(self, input):
+        if not self.log:
+            for k in input:
+                self.log[k] = [input[k]]
+        else:
+            for k in input:
+                self.log[k].append(input[k])
+
+        if self.print_format is not None:
+            print(self.print_format.format(**input))
+
+    def __len__(self):
+
+        values = self.log.values()
+        if len(values):
+            return 0
+
+        return len(next(iter(values)))
