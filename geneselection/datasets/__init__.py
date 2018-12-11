be677d3f9e61cede3c80fd1710084250b5e1917b
heeler <jamies@alleninstitute.org>
2018-12-11 14:41:11 -0800

    changed init to use __all__ mechanism

--- geneselection/datasets/__init__.py
+++ geneselection/datasets/__init__.py
@@ -1,1 +1,1 @@
-from .dataset import *
+__all__ = ['correlated_random_variables', 'dataset', 'mouse_retina', 'scrnaseq_cardio_20181129']
---
+++ geneselection/solvers/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['ae', 'basic_net_trainer', 'bvae']
