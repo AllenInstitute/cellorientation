d81cc247eb0df248027844413ea097aee423d3d2
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-02-06 13:51:36 -0800

    make persistence plot for hub spoke data

--- geneselection/solvers/enet_pca.py
+++ geneselection/solvers/enet_pca.py
@@ -255,3 +255,31 @@ def parallel_runs(
     pool.join()
 
     return result_list
+
+
+def hub_persistence_plot(
+    adata, boot_results, downsample_backgound=1000, width=400, height=400
+):
+
+    z = np.stack([br["beta"] for br in boot_results])
+    sel_fracs = (z != 0).mean(axis=0)
+    df_sel = tidy(sel_fracs)
+    df_sel.columns = ["Gene Index", "Lambda Index", "Selection Fraction"]
+
+    df_tmp = adata.var.copy()
+    df_tmp["Gene Index"] = df_tmp.index.values.astype(np.int64)
+    df_sel = df_sel.merge(df_tmp)
+    df_sel = df_sel[df_sel["Gene Index"] < 1000 + downsample_backgound]
+
+    chart = (
+        alt.Chart(df_sel, width=width, height=height)
+        .mark_line(opacity=0.25, interpolate="linear", color="blue")
+        .encode(
+            x="Lambda Index:Q",
+            y="Selection Fraction:Q",
+            detail="Gene Index:N",
+            color="Type:N",
+        )
+    )
+
+    return chart
